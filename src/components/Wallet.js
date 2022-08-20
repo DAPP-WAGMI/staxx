1daf040517b60609a23f62148a97a0b65c2f0233
Sheena Johns <smjohns710@gmail.com>
2022-08-20 16:34:51 -0400

    Add wallet connect

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,5 +1,43 @@
 import React, { useEffect } from 'react'
-import { ethers } from 'ethers'
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { ethers } from 'ethers';
+import Web3Modal from "web3modal";
+
+
+
+//End of WalletConnect Ether.js code
+
+//Beginner of provider events
+// Subscribe to accounts change
+// provider.on("accountsChanged", (accounts: string[]) => {
+//   console.log(accounts);
+// });
+
+// // Subscribe to chainId change
+// provider.on("chainChanged", (chainId: number) => {
+//   console.log(chainId);
+// });
+
+// // Subscribe to provider connection
+// provider.on("connect", (info: { chainId: number }) => {
+//   console.log(info);
+// });
+
+// // Subscribe to provider disconnection
+// provider.on("disconnect", (error: { code: number; message: string }) => {
+//   console.log(error);
+// });
+
+//End of Provider Events
+
+//Beginning of WalletConnect-ethers-example
+
+
+import { Contract, providers, utils } from "ethers";
+
+
+//End of WalletConnect-ethers-example
+
 import { useLazyQuery } from '@apollo/client'
 import { GET_PROFILES } from '../utils/queries'
 import LensHub from '../abi/LensHub.json'
@@ -9,6 +47,48 @@ import Login from './Login'
 function Wallet({ setProfiles = () => {}, ...props }) {
 const { wallet, setWallet, setLensHub, authToken, setProvider } = useWallet()
   const [getProfiles, profiles] = useLazyQuery(GET_PROFILES)
+
+  
+  async function connect() {
+    
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider, // required
+        options: {
+          infuraId: "INFURA_ID" // required
+        }
+      }
+    };
+
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions // required
+    });
+
+    const instance = await web3Modal.connect();
+
+    const provider = new ethers.providers.Web3Provider(instance);
+
+    const signer = provider.getSigner();
+    // if (!process.env.REACT_APP_INFURA_ID) {
+    //   throw new Error("Missing Infura Id");
+    // }
+    const web3Provider = new WalletConnectProvider({
+      infuraId: "26JsrucCKgQZxquUitySbZrwAFq",
+    });
+
+    // web3Provider.on("disconnect", reset);
+
+    const accounts = (await web3Provider.enable());
+    // setAddress(accounts[0]);
+    // setChainId(web3Provider.chainId);
+
+    setProvider(provider);
+    const address = await signer.getAddress()
+
+    setWallet({...wallet, signer, address})
+  }
 
   useEffect(() => {
     if (!authToken || !wallet.address) return;
@@ -34,32 +114,45 @@ const { wallet, setWallet, setLensHub, authToken, setProvider } = useWallet()
 
   }, [profiles.data])
 
-  const connectWallet = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner()
-    const address = await signer.getAddress()
+  // const connectWallet = async () => {
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum)
+  //   await provider.send("eth_requestAccounts", []);
+  //   const signer = provider.getSigner()
+  //   const address = await signer.getAddress()
 
-    const contract = new ethers.Contract('0x60Ae865ee4C725cd04353b5AAb364553f56ceF82', LensHub, signer)
-    setLensHub(contract)
+  //   const contract = new ethers.Contract('0x60Ae865ee4C725cd04353b5AAb364553f56ceF82', LensHub, signer)
+  //   setLensHub(contract)
   
-    provider.getBalance(address).then((balance) => {
-      const balanceInEth = ethers.utils.formatEther(balance)
-      console.log({balanceInEth})
-      setWallet({...wallet, signer, address, balanceInEth})
-      })
-  }
+  //   provider.getBalance(address).then((balance) => {
+  //     const balanceInEth = ethers.utils.formatEther(balance)
+  //     console.log({balanceInEth})
+  //     setWallet({...wallet, signer, address, balanceInEth})
+  //     })
+  // }
+//Beginning of WalletConnect Staxx - ETHMEX
 
-  useEffect(() => {
-    connectWallet()
-  }, [])
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: "INFURA_ID" // required
+    }
+  }
+};
+// Ending of WalletConnect Staxx - ETHMEX
+
+
+  // useEffect(() => {
+  //   connectWallet()
+  // }, [])
   
   return (
     
     <div>
     { wallet.signer ? 
     <Login/> : 
-    <button onClick={connectWallet} >Connect Wallet</button>
+    <button onClick={connect} >Connect Wallet</button>
     }
   </div>
   );
